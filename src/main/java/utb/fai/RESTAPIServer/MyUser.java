132ca0258594b8981bf0af28e2af6f9c9756c27f
Hspsr <119581613+Hspsr@users.noreply.github.com>
2024-11-25 22:59:21 +0100

    Update MyUser.java

--- src/main/java/utb/fai/RESTAPIServer/MyUser.java
+++ src/main/java/utb/fai/RESTAPIServer/MyUser.java
@@ -1,11 +1,20 @@
 package utb.fai.RESTAPIServer;
 
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "users")
 public class MyUser {
-
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 100)
     private String name;
+    @Column(nullable = false, unique = true, length = 100)
     private String email;
+    @Column(nullable = false, length = 100)
     private String phoneNumber;
 
     public MyUser() {}
@@ -17,11 +26,48 @@ public class MyUser {
     }
 
     public boolean isUserDataValid() {
-        // Add your validation logic here (e.g., email and phone number format
-        // validation)
+        if (name == null || name.trim().isEmpty()){
+            return false;
+        }
+        if(phoneNumber == null || !phoneNumber.matches("\\+?[0-9]*")){
+            return false;
+        }
+        if(email == null || !email.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")){
+            return false;
+        }
         return true;
     }
 
-    // TODO: Getters and setters
+    public Long getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+
+    public void setID(Long id){
+        this.id = id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phonenumber){
+        this.phoneNumber = phonenumber;
+    }
     
 }
